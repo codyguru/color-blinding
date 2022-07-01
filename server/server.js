6168fac19b5cdc88b002f46473aec2ec907161b6
codyguru <codymaster.prof@gmail.com>
2022-06-30 23:36:23 -0700

    removed cors

--- server/server.js
+++ server/server.js
@@ -2,13 +2,11 @@ const path = require("path");
 const connectDB = require("../backend/connnection");
 const express = require("express");
 const app = express();
-const cors = require("cors");
 
 //AUTOMATICALLY SERVERS INDEX PAGE for app.get("/")
 const publicPath = path.join(__dirname, "..", "/build");
 console.log(publicPath);
 
-app.use(cors());
 app.use(express.json());
 app.use(express.static(publicPath));
 
