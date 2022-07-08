fc8cf340028e6e0450d369f50a6ddd3aa418c0d7
codyguru <codymaster.prof@gmail.com>
2022-07-07 23:23:10 -0700

    good for deployment

--- backend/database/connection.js
+++ backend/database/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const connectDB = async () => {
   await mongoose.connect(process.env.MONGODB_URI || process.env.URI, {
     useNewUrlParser: true,
+    useCreateIndex: true,
     useUnifiedTopology: true,
   });
-  console.log("MongoDB has been connected!");
+  console.log("MongoDB has been connected");
 };
 
 module.exports = connectDB;
---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import App from './components/App';
+
+// // CSS
+// import "normalize.css/normalize.css"
+// import "./styles/styles.scss";
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <App />
+// );
